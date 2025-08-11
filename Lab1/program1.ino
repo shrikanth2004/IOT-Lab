2025897948b4040fdb22e9251b3e5916e413e680
shrikanth nayak <159421017+shrikanth2004@users.noreply.github.com>
2025-08-11 10:35:19 +0530

    Create program1.ino

---
+++ Lab1/program1.ino
@@ -0,0 +1,34 @@
+// C++ code
+//
+const int ledpins[]={1,2,3,4,5};
+const int numleds=5;
+
+void setup()
+{
+  for(int i=0;i<numleds;i++){
+  pinMode(ledpins[i], OUTPUT);
+}
+}
+
+void loop()
+{
+    for(int i=0;i<numleds;i++){
+      
+
+  digitalWrite(ledpins[i], HIGH);
+  delay(250); 
+  digitalWrite(ledpins[i], LOW);
+  delay(250);
+    
+    }
+  for(int i=0;i<numleds;i++){
+
+    digitalWrite(ledpins[i], HIGH);
+  delay(250);
+  digitalWrite(ledpins[i], LOW);
+          
+
+  delay(250);
+          
+  }
+}
